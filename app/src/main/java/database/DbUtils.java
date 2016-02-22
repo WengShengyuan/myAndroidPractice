aa87e977363bdcf3fbaa962dfd8c1ca09f6c9999
wengshengyuan <wengshengyuan@gmail.com>
2016-02-22 21:55:28 +0800

    #

--- app/src/main/java/database/DbUtils.java
+++ app/src/main/java/database/DbUtils.java
@@ -11,7 +11,7 @@ import java.io.File;
 public class DbUtils {
 
     private volatile static DbUtils instance;
-
+// etsetgit
     public static DbUtils getInstance() {
         if (instance == null) {
             synchronized (DbUtils.class) {
